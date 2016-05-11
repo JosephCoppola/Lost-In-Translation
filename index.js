5971707f1bf10a1946c3a693c0380a2d53f34e7e
JosephCoppola <jjc9718@rit.edu>
2016-05-11 09:50:45 -0400

    Server loading in html page and serving it now. Setting up webpack to work with es6

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-import express from 'express';
-import bodyParser from 'body-parser';
 import http from 'http';
+import fs from 'fs';
+
+const index = fs.readFileSync(__dirname + '/client/client.html');
 
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.write(index);
+  res.end();
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
