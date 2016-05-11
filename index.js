490a309e0f4989917928ef6df90d722011b871ce
JosephCoppola <jjc9718@rit.edu>
2016-05-11 12:50:42 -0400

    Server serves webpage now

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import fs from 'fs';
 const index = fs.readFileSync(__dirname + '/client/client.html');
 
 http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, {'Content-Type': 'text'});
   res.write(index);
   res.end();
 }).listen(1337, '127.0.0.1');
