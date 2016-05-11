25fef17d0eab6a469cc7b7f4c20ff8fda71beaa6
JosephCoppola <jjc9718@rit.edu>
2016-05-11 19:43:05 -0400

    Update server to use express, serving js from build folder now

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-import http from 'http';
 import fs from 'fs';
+import express from 'express';
+import path from 'path';
+
+const server = express();
 
 const index = fs.readFileSync(__dirname + '/client/client.html');
+const js = fs.readFileSync(__dirname + '/build/js/app.js');
 
-http.createServer((req, res) => {
+server.use('**/js', express.static(path.resolve(`${__dirname}/build/js`)));
+
+server.get('/', (req, res) => {
   res.writeHead(200, {'Content-Type': 'text'});
   res.write(index);
   res.end();
-}).listen(1337, '127.0.0.1');
+});
+
+server.listen(1337);
 
 console.log('Server running at http://127.0.0.1:1337/');
