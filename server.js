27691b22630e5a7dae1f7824c4b8e7d7b7663d0b
Tyler Sargent <github@sargent.io>
2016-05-12 21:12:04 -0400

    Added media to server

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const index = fs.readFileSync(__dirname + '/client/client.html');
 
 server.use('**/js', express.static(path.resolve(`${__dirname}/build/js`)));
 server.use('**/css', express.static(path.resolve(`${__dirname}/client/stylesheets`)));
+server.use('**/media', express.static(path.resolve(`${__dirname}/client/media`)));
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 
